68c3b2d02897a25f94833d453f840277df075e05
yoobin <yoobin@ttadag.com>
2016-03-17 12:48:08 +0900

    npm, bower모듈 정리 및 angular-translate package testing.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ttadag inc
+
+npm node package management
+ - express, grunt, jasmine, karma, etc...
+ 
+bower client package management
+ - angular, angular-material, angular-translate, etc...
--- app/app.module.js
+++ app/app.module.js
@@ -5,7 +5,22 @@
 	 * @description
 	 * APP 의존성 init관련
 	 */
-	angular.module('TTADAGAPP', ['ngMaterial']);
+	angular.module('TTADAGAPP', ['ngMaterial', 'pascalprecht.translate']);
+
+	angular.module('TTADAGAPP').config(['$translateProvider', function($translateProvider) {
+		$translateProvider.translations('en', {
+			'TITLE': 'Hello'
+		});
+
+		$translateProvider.translations('ko', {
+			'TITLE': '안녕하세요'
+		});
+
+		$translateProvider.preferredLanguage('ko');
+		$translateProvider.useSanitizeValueStrategy('escape');
+	}]);
+
+
 	angular.module('TTADAGAPP')
 		.controller('APPController', ['$scope', function($scope) {
 			$scope.start = function() {
